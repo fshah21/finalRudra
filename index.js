6904b3887f2d2970b732b8303f13ca3eeff5bc2b
unknown <feyashah21@gmail.com>
2024-07-09 16:24:12 +0530

    use cors

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const { sequelize, User } = require('./models/index');
+const cors = require('cors');
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
